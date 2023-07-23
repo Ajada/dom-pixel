7145484483567135d77425a542aa0dd65993d171
Ajada <gonsalezajada@gmail.com>
2023-07-23 12:38:55 -0300

    Corrigindo titulo da página

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -16,7 +16,7 @@ defineProps({
 
     <template #header>
       <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-        Dashboard
+        Catálogo de Produtos
       </h2>
     </template>
 
